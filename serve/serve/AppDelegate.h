a062814b73fb4ca198199b0f6b740af0a091e32f
Prayaas Jain <prayaas.jain@pearson.com>
2015-04-28 21:35:55 -0700

    first commit

---
+++ serve/serve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  serve
+//
+//  Created by Prayaas Jain on 4/28/15.
+//  Copyright (c) 2015 prayaasjain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
